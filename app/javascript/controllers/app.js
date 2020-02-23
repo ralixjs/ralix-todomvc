aa37b762173c4f7aeb35572c1f5aa1945db17064
Lautaro Castillo Barranco <lautarocastillo.93@gmail.com>
2020-02-23 18:49:59 +0100

    All working

--- app/javascript/controllers/app.js
+++ app/javascript/controllers/app.js
@@ -5,62 +5,67 @@ export default class AppCtrl {
 
   enterSubmit() {
     if (currentEvent().keyCode === 13 && currentElement().value != '') {
-      let todo = getList('all')
-      addItem(todo.length, '', currentElement().value)
-      const obj = { id: `${todo.length}`, class: '', value: currentElement().value }
+      let todo    = getList('all')
+      const id    = Date.now().toString()
+      const value = currentElement().value
+      const obj   = { id: `${id}`, class: '', value: value }
+
       todo.push(obj)
       setList(todo)
       currentElement().value = ''
       reloadLeft()
+
+      if (find('.selected').text != 'Completed')
+        addItem(id, '', value)
     }
   }
 
-  checkTask(id){
-    const li = find(`#li_${id}`)
-    addClass(li, 'completed' )
-
-    let input = find(`#li_${id} input`)
-
-    if (input.checked)
-    input.checked = false
-    else
-    input.checked = true
-
+  toggleCheck(id){
+    toggleClass(`#li_${id}`, 'completed')
+    find(`#li_${id} input`).remove()
+    insertHTML(`#li_${id} div`, checkBox(id, find(`#li_${id}`).className), 'begin')
     let todo = getList('all')
-    todo[li.id.replace('li_', '')]['class'] = 'completed'
-    setList(todo)
-    reloadLeft()
+
+    todo.forEach((obj, index) => {
+      if (obj.id == id.replace('li_', '')){
+        todo[index]['class'] = find(`#li_${id}`).className
+        setList(todo)
+        reloadLeft()
+      }
+    })
   }
 
   destroy(id) {
     find(`#li_${id}`).remove()
     let todo = getList('all')
+
     todo.forEach((obj, index) => {
       if (obj.id == id.replace('li_', ''))
-      todo.splice(index, 1)
+        todo.splice(index, 1)
     })
     setList(todo)
     reloadLeft()
   }
 
   addItem(id, completed, value) {
+    insertHTML('ul', `<li id="li_${id}" class="${completed}"><div class="view">${this.checkBox(id, completed)}<label>${value}</label><button class="destroy" onclick="destroy('${id}')"></button></div></li>`, 'end')
+  }
+
+  checkBox(id, completed) {
     const checked = completed != '' ? 'checked' : ''
-    insertHTML('ul', `<li id="li_${id}" class="${completed}"><div class="view"><input class="toggle" type="checkbox" onclick="checkTask('${id}')" ${checked}><label>${value}</label><button class="destroy" onclick="destroy('${id}')"></button></div></li>`, 'end')
+    return `<input class="toggle" type="checkbox" onclick="toggleCheck('${id}')" ${checked}>`
   }
 
   clearCompleted() {
-    insertHTML('ul', '')
-    const todo = getList('active')
-    setList(todo)
-    todo.forEach((obj) => {
-      this.addItem(obj.id, obj.class, obj.value)
-    })
+    findAll('.completed').forEach((obj) => { obj.remove() })
+    setList(getList('active'))
   }
 
   loadList(list) {
     removeClass('.selected', 'selected')
     insertHTML('ul', '')
     addClass(`a[onclick="loadList('${list}')"]`, 'selected')
+
     this.getList(list).forEach((obj) => {
       this.addItem(obj.id, obj.class, obj.value)
     })
@@ -69,10 +74,11 @@ export default class AppCtrl {
 
   reloadLeft() {
     const left = this.getList('active').length
+
     if (left)
-    insertHTML('.todo-count', `${left} items left`)
+      insertHTML('.todo-count', `<strong>${left} items left</strong>`)
     else
-    insertHTML('.todo-count', '')
+      insertHTML('.todo-count', '')
   }
 
   getList(list) {
