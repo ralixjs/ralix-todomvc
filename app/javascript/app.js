6c666703cd848ef004d833773ff161d7b0f90aea
Marc Anguera Insa <srmarc.ai@gmail.com>
2020-04-11 18:14:46 +0200

    Ralix Templates (#3)

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,13 +1,13 @@
-// Dependencies
 import { RalixApp } from 'ralix'
 
-// Controllers
-import AppCtrl      from 'controllers/app'
+import AppCtrl from 'controllers/app'
+import * as Templates from 'templates'
 
 const App = new RalixApp({
   routes: {
     '/.*': AppCtrl
-  }
+  },
+  templates: Templates
 })
 
 App.start()
